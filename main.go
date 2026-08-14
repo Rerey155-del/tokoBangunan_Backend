8abd99211e61b3d334f110fdd9095870a916673e
Rerey <144551736+Rerey155-del@users.noreply.github.com>
2026-08-14 16:28:20 +0700

    refactor: remove user endpoints and update product creation logic

--- main.go
+++ main.go
@@ -163,6 +163,112 @@ func main() {
 		})
 	})
 
+	//  Bikin data users
+
+	mux.HandleFunc("POST /users", func(w http.ResponseWriter, r *http.Request) {
+		var u User
+		if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+			http.Error(w, "format Request User Salah", http.StatusBadRequest)
+			return
+		}
+
+		res, err := db.Exec("INSERT INTO user (name, email, password)", u.Name, u.Email, u.Password)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		lastId, _ := res.LastInsertId()
+		u.ID = int(lastId)
+		u.Password = ""
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(u)
+		w.WriteHeader(http.StatusCreated)
+
+	})
+
+	// Read all users
+	mux.HandleFunc("GET /users", func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT id name, email FROM users")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		var users []User
+		for rows.Next() {
+			var u User
+			if err := rows.Scan(&u.ID, &u.Name, &u.Email); err != nil {
+				continue
+			}
+			users = append(users, u)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(users)
+	})
+
+	// Read User berdasarkan ID + Produk miliknya
+	mux.HandleFunc("GET /users/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, _ := strconv.Atoi(r.PathValue("id"))
+
+		var u User
+		err := db.QueryRow("SELECT id, name, email FROM users WHERE id = ?", id).Scan(&u.ID, &u.Name, &u.Email)
+
+		if err == sql.ErrNoRows {
+			http.Error(w, "User tidak ditemukan", http.StatusNotFound)
+			return
+		} else if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(u)
+	})
+
+	// update user (PUT/ users/{id})
+	mux.HandleFunc("PUT /users/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, _ := strconv.Atoi(r.PathValue("id"))
+		var u User
+		if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+			http.Error(w, "", http.StatusBadRequest)
+			return
+		}
+		res, err := db.Exec("UPDATE users SET name = ? , email = ? WHERE id = ?", u.Name, u.Email, id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		rowsAffected, _ := res.RowsAffected()
+		if rowsAffected == 0 {
+			http.Error(w, "User tidak ditemukan", http.StatusNotFound)
+			return
+		}
+		u.ID = id
+		u.Password = ""
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(u)
+
+	})
+
+	// Delete User (Delete/users/{id})
+	mux.HandleFunc("DELETE /users/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, _ := strconv.Atoi(r.PathValue("id"))
+
+		res, err := db.Exec("DELETE FROM users WHERE id = ?", id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		rowsAffected, _ := res.RowsAffected()
+		if rowsAffected == 0 {
+			http.Error(w, "User tidak ditemukan", http.StatusNotFound)
+			return
+		}
+		w.WriteHeader(http.StatusNoContent)
+	})
+
 	// 1. READ ALL (Mendapatkan semua produk)
 	mux.HandleFunc("GET /products", func(w http.ResponseWriter, r *http.Request) {
 		rows, err := db.Query("SELECT id, name, price FROM products")
